10c9187441e6a094f75b0bc5ad75ae7bd9fda307
Syahrul <syahrulataufikaja11@gmail.com>
2021-11-19 03:13:47 +0700

    test on s3

--- routes/absenRoute.js
+++ routes/absenRoute.js
@@ -4,7 +4,7 @@ import imagePath from '../constanta/pathImageConst.js'
 import path from 'path'
 const db = fb.firestore()
 import { fileURLToPath } from 'url'
-import {faceapi as faceApi, canv as canvas, imageUpload, noneUpload, uploadS3} from '../utils/imagesProcessing.js'
+import { faceapi as faceApi, canv as canvas, imageUpload, noneUpload, uploadS3, downloadS3 } from '../utils/imagesProcessing.js'
 const user = db.collection('tm_user')
 const router = express.Router()
 
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 router.post('/absen/check', imageUpload(imagePath.ABSEN).single('file'), async (req, res) => {
     const body = req.body
     // Load the face detection models   
-    const image = await canvas.loadImage(path.join(__dirname,`../images/${req.file.filename}`))
+    const image = await canvas.loadImage(path.join(__dirname, `../images/${req.file.filename}`))
     const labeledFaceDescriptors = await labeledImagesFix
     const faceMatcher = new faceApi.FaceMatcher(labeledFaceDescriptors, 0.5)
     const singleResult = await faceApi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor()
@@ -22,32 +22,32 @@ router.post('/absen/check', imageUpload(imagePath.ABSEN).single('file'), async (
     if (singleResult) {
         bestMatch = await faceMatcher.findBestMatch(singleResult.descriptor)
     }
-    if(bestMatch){
+    if (bestMatch) {
         const id = bestMatch._label
         let data = null
         await user.doc(id).get()
-        .then(doc => {
-            if(doc.exists){
-                data = doc.data()
-                data.id = doc.id
-            }
-        })
-        if(!data){
-            res.status(401).json({
-                "success":false,
-                "errorMessage":"Wajah tidak dikenali harap lapor ke Admin !"
+            .then(doc => {
+                if (doc.exists) {
+                    data = doc.data()
+                    data.id = doc.id
+                }
             })
-        }else{
+        if (!data) {
+            res.status(401).json({
+                "success": false,
+                "errorMessage": "Wajah tidak dikenali harap lapor ke Admin !"
+            })
+        } else {
             await db.collection('tt_absensi').add({
-                suhu:body.suhu,
-                path:req.file.path,
-                userId:data.id,
-                createdDate:new Date(),
-                nama:data.nama
+                suhu: body.suhu,
+                path: req.file.path,
+                userId: data.id,
+                createdDate: new Date(),
+                nama: data.nama
             })
             res.json({
-                "success":true,
-                "obj":data
+                "success": true,
+                "obj": data
             })
         }
 
@@ -57,7 +57,29 @@ router.post('/absen/check', imageUpload(imagePath.ABSEN).single('file'), async (
 
 router.post('/absen/test', noneUpload.single('image'), async (req, res) => {
     const file = req.file
-    const result  = await uploadS3(file)
+    const result = await uploadS3(file)
+    console.log(result)
     res.send('OK')
+})
+
+router.get('/absen/get/:key', async (req, res) => {
+    console.log('login slur')
+    const key = req.params.key
+    const stream = await downloadS3(key)
+    res.set('Content-Type', 'image/png')
+    stream.pipe(res);
+
+    // stream.on('open', function () {
+    //     console.log('open cuy')
+    //     res.set('Content-Type', 'image/png');
+    // })
+    // stream.on('error', function () {
+    //     console.log('error cuy')
+    //     res.set('Content-Type', 'text/plain');
+    //     res.status(404).end('Not found');
+    // })
+    // res.send('default euy')
+
+
 })
 export default router
--- utils/imagesProcessing.js
+++ utils/imagesProcessing.js
@@ -9,8 +9,9 @@ import { fileURLToPath } from 'url'
 
 import env from 'dotenv'
 env.config()
+import pkg from 'aws-sdk';
+const { S3 } = pkg;
 
-import {S3} from 'aws-sdk'
 const bucket = process.env.AWS_BUCKET_NAME
 const region = process.env.AWS_BUCKET_REGION
 const accessKeyId = process.env.AWS_ACCESS_KEY
@@ -18,24 +19,32 @@ const secretAccessKey = process.env.AWS_SECRET_KEY
 const s3 = new S3({
   region,
   accessKeyId,
-  secretAccessKey
+  secretAccessKey,
+  endpoint: 'https://objects-us-east-1.dream.io'
 })
 
 export const uploadS3 = (file) => {
   const fileStream = fs.createReadStream(file.path)
   const uploadParams = {
-    Bucket:bucket,
-    Body:fileStream,
-    Key:file.filename
+    Bucket: bucket,
+    Body: fileStream,
+    Key: file.filename
   }
   return s3.upload(uploadParams).promise()
 }
 
+export const downloadS3 = (key) => {
+  const downloadParams = {
+    Key:key,
+    Bucket:bucket
+  }
+  return s3.getObject(downloadParams).createReadStream()
+}
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const {Canvas, Image, ImageData} = canvas
-faceApi.env.monkeyPatch({Canvas, Image, ImageData, fetch:fetch})
+const { Canvas, Image, ImageData } = canvas
+faceApi.env.monkeyPatch({ Canvas, Image, ImageData, fetch: fetch })
 
 
 await faceApi.nets.faceRecognitionNet.loadFromDisk(path.join(__dirname, '../models'))
@@ -45,11 +54,11 @@ export const faceapi = faceApi
 export const canv = canvas
 
 const imageStorage = multer.diskStorage({
-    destination: 'images', 
-    filename: (req, file, cb) => {
-          cb(null, file.fieldname + '_' + Date.now() 
-             + path.extname(file.originalname))
-    }
+  destination: 'images',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now()
+      + path.extname(file.originalname))
+  }
 });
 
 const upload = (obj) => {
@@ -57,41 +66,41 @@ const upload = (obj) => {
 }
 
 const makeid = (length) => {
-  var result           = '';
-  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   var charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return result;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
 }
 
-export const noneUpload = multer({dest:'uploads/'})
-export const imageUpload = (prop = {}) =>{
+export const noneUpload = multer({ dest: 'uploads/' })
+export const imageUpload = (prop = {}) => {
   const uniqueCode = makeid(10)
   const storage = multer.diskStorage({
-    destination:prop.path,
+    destination: prop.path,
     filename: (req, file, callback) => {
       const filesDir = `${prop.path}/${uniqueCode}`
       if (!fs.existsSync(filesDir)) {
-            fs.mkdirSync(filesDir);
-        }
+        fs.mkdirSync(filesDir);
+      }
       callback(null, `${uniqueCode}/${new String(file.originalname).replace(/\s/g, '')}`)
     }
   })
   const obj = {
-      storage: storage,
-      limits: {
-        fileSize: 10000000 // 10000000 Bytes = 10 MB
-      },
-      fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) { 
-          // upload only png and jpg format
-          return cb(new Error('Please upload a Image'))
-        }
-        req.body.code = uniqueCode
-        console.log(`unique Code  : ${uniqueCode}`)
+    storage: storage,
+    limits: {
+      fileSize: 10000000 // 10000000 Bytes = 10 MB
+    },
+    fileFilter(req, file, cb) {
+      if (!file.originalname.match(/\.(png|jpg|jpeg)$/)) {
+        // upload only png and jpg format
+        return cb(new Error('Please upload a Image'))
+      }
+      req.body.code = uniqueCode
+      console.log(`unique Code  : ${uniqueCode}`)
       cb(undefined, true)
     }
   }
